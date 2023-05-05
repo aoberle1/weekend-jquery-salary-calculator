6546dbf03d8ebc4ae76c4fed191765ba5d4fbcc0
Adam Oberle <oberle.adam@gmail.com>
2023-05-05 17:17:04 -0500

    added inputs for 5 fields, created function to add input data into new data cells in a new row in our table

--- script.js
+++ script.js
@@ -3,4 +3,49 @@ $(document).ready(onReady);
 function onReady() {
     console.log('We have jQuery! 💲💲💲');
 
+    $('#submitButton').on( 'click', handleSubmit);
+
+}
+
+function handleSubmit(event) {
+    
+    let inputFname = $('#input-f-name').val();
+    let inputLname = $('#input-l-name').val();
+    let inputIdNum = $('#input-id-Num').val();
+    let inputJobTitle = $('#input-job-Title').val();
+    let inputAnnualSalary = $('#input-annSalary').val();
+
+
+    event.preventDefault();
+
+    $('#table-body').append(`
+        <tr>
+            <td>
+                ${inputFname}
+            </td>
+            <td>
+                ${inputLname}
+            </td>
+            <td>
+                ${inputIdNum}
+            </td>
+            <td>
+                ${inputJobTitle}
+            </td>
+            <td>
+                ${inputAnnualSalary}
+            </td>
+            <td>
+                <button class="delete-btn">delete</button>
+            </td>
+        </tr>
+    `)
+
+    $('#input-f-name').val('');
+    $('#input-l-name').val('');
+    $('#input-id-Num').val('');
+    $('#input-job-Title').val('');
+    $('#input-annSalary').val('');
+
+
 }
