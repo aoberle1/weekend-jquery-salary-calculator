12aac05e47bed9aaa05492f17a4612c30f08f442
Adam Oberle <oberle.adam@gmail.com>
2023-05-05 19:32:22 -0500

    connected Monthly Salary to js so it can be appended

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ $(document).ready(onReady);
 function onReady() {
     console.log('We have jQuery! 💲💲💲');
 
+    // target element with id submitButton and on click, run function handleSubmit
     $('#submitButton').on( 'click', handleSubmit);
 
 }
 
+// function that runs on event (click of submit button)
 function handleSubmit(event) {
     
+    // creating variables to set as the values of the form inputs
     let inputFname = $('#input-f-name').val();
     let inputLname = $('#input-l-name').val();
     let inputIdNum = $('#input-id-Num').val();
     let inputJobTitle = $('#input-job-Title').val();
     let inputAnnualSalary = $('#input-annSalary').val();
-
+    
+    // creating variable to store monthly salary value
+    let monthlySalary = inputAnnualSalary / 12;
 
     event.preventDefault();
 
+    // when the function runs, targeting the element with id table-body, and append it to include
+    // the following table row and table data cells
     $('#table-body').append(`
         <tr>
             <td>
@@ -41,6 +48,14 @@ function handleSubmit(event) {
         </tr>
     `)
 
+    $('#monthly-salary-span').empty();
+    monthlySalaryContainer++
+
+    // targets element with id monthly-salary-span and appends it to the value of the
+    // monthlySalaryContainer variable
+    $('#monthly-salary-span').append(monthlySalaryContainer)
+    
+    // when submit button is clicked, empties out the form inputs on the html
     $('#input-f-name').val('');
     $('#input-l-name').val('');
     $('#input-id-Num').val('');
@@ -49,3 +64,5 @@ function handleSubmit(event) {
 
 
 }
+
+let monthlySalaryContainer = 0;
